23d215b644186ebe9b551ef48aed8f138f4afdb2
ChrisRivera276 <c.rivera07@outlook.com>
2022-04-12 15:06:02 -0400

    comit

--- JS/site.js
+++ JS/site.js
@@ -1,19 +1,50 @@
 // Step One - controller accept request
 function getMessage() {
     let msg = document.getElementById("txtmessage").value;
-    displayMessage(msg);
+    
+    // validate if the msg not equal to empty string
+    if (msg != '') {
+       msg = msg.toLowerCase();
+        let revMessage = reverseMessageB(msg);
+        displayMessage(revMessage);
+        
+    } else {
+       alert("You must enter a string")
+    }
 }
+
+
+
+function reverseMessageA(msg) {
+    let revMessage = "";
+
+    // decrimenting loop
+    // 0 1 2 3 4
+    // B o b b y
+    for (let index = msg.length - 1; index >= 0; index--) {
+        let char = msg[index];
+        revMessage += char;
+    }
+
+    return revMessage;
+}
+
+function reverseMessageB(msg){
+
+    let revMessage = "";
+    revMessage = msg.split("").reverse().join("");
+    
+    return revMessage;
+}
+
+
+
+
+
 // Final Step - view
 function displayMessage(message) {
 
     //fist get ol element from page
     element = document.getElementById("results");
-    //next create new li element
-    let item = document.createElement("li");
-    //add classes to li element
-    item.classList.add("list-group-item");
-    // set message for li element
-    item.innerHTML = message;
-    // add new item to list
-    element.appendChild(item);
+    element.innerHTML = message;
 }
